9db42275fd3a67a85f7d018ec9f78294b4c049a9
Aleksey <jetta261@gmail.com>
2021-12-21 16:16:53 +0300

    dotenv

--- settings/dotenvs.py
+++ settings/dotenvs.py
@@ -1,6 +1,13 @@
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
+import os
 
 def load_env_values():
     env_path = Path.cwd() / 'settings' / '.env'
     return load_dotenv(env_path) if env_path.exists() else False
+
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
